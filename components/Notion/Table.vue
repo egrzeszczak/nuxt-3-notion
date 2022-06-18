4637671d21462498a433a2abb66806ffd13a568e
egrzeszczak <ernest.grzeszczak@protonmail.com>
2022-06-18 20:16:59 +0200

    fix: :bug: table overflowing fix, style change

--- components/Notion/Table.vue
+++ components/Notion/Table.vue
@@ -1,18 +1,29 @@
 <script setup>
-const props = defineProps(['block'])
+const props = defineProps(["block"]);
 const { data: content } = await useFetch(
-    `/api/notion/retrieve-block-children/${props.block.id}`
-)
+	`/api/notion/retrieve-block-children/${props.block.id}`
+);
 </script>
 
 <template>
-    <table class="notion-table">
-        <tbody class="notion-table-body">
-            <tr class="notion-table-tr" v-for="(tableRow, rowIndex) in content.results" :key="rowIndex">
-                <td class="notion-table-td" v-for="(cell, cellIndex) in tableRow[tableRow.type].cells" :key="cellIndex">
-                    <NotionRichText :richText="cell" />
-                </td>
-            </tr>
-        </tbody>
-    </table>
+	<div class="overflow-x-auto">
+		<table class="notion-table">
+			<tbody class="notion-table-body">
+				<tr
+					class="notion-table-tr"
+					v-for="(tableRow, rowIndex) in content.results"
+					:key="rowIndex"
+				>
+					<td
+						class="notion-table-td"
+						v-for="(cell, cellIndex) in tableRow[tableRow.type]
+							.cells"
+						:key="cellIndex"
+					>
+						<NotionRichText :richText="cell" />
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </template>
