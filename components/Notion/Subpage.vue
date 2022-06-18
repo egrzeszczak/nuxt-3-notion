8fb86b9ab55d351d7fce3c99938b64d33f86c1bb
egrzeszczak <ernest.grzeszczak@protonmail.com>
2022-06-18 21:49:42 +0200

    feat: :sparkles: subpages will now render with lazyFetch

--- components/Notion/Subpage.vue
+++ components/Notion/Subpage.vue
@@ -1,16 +1,18 @@
 <template>
-	<NuxtLink :to="`/page/${block.id}`" class="notion-subpage">
+	<NuxtLink v-if="!pending" :to="`/page/${block.id}`" class="notion-subpage">
 		<span class="flex items-center gap-2 notion-subpage-text">
 			<NotionIcon :icon="header.icon" />
 			{{ block.child_page.title }}
 			&neArr;</span
 		>
 	</NuxtLink>
+	<Skeleton v-else class="h-8" />
 </template>
 
 <script setup>
 const props = defineProps(["block"]);
-const { data: header } = await useFetch(
+const { pending, data: header } = await useLazyFetch(
 	`/api/notion/retrieve-page/${props.block.id}`
 );
+watch(header, (newHeader) => {});
 </script>
